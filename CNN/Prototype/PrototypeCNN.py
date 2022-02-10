a8b59ca388f30464427a55b4892d1d99cf5b2ce5
JosephJ42 <josephjarvis42@gmail.com>
2022-02-10 12:23:04 +0000

    Inital CNN prototype implementation for 2 different houseplants

--- CNN/Prototype/PrototypeCNN.py
+++ CNN/Prototype/PrototypeCNN.py
@@ -3,14 +3,57 @@
 #
 #
 
-#TensorFlow Imports
+#Tensorflow related Imports
 import tensorflow as tf
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.utils import plot_model
 
-#Misc Imports
+#Misc imports
 import matplotlib.pyplot as plt
+import cv2
+import os
+import numpy as np
+from tqdm import tqdm
+import random
 
 
+#import numpy array data from their files
 
+X = np.load('D:/UWE-DSP/CNN/Prototype/features.npy')
+y = np.load('D:/UWE-DSP/CNN/Prototype/labels.npy')
 
-#import data from 
+#normalise the data
+trainingX = X/255.0
+
+#begin to build the CNN
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(180, 180, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+
+model.summary()
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10))
+model.add(Activation('sigmoid'))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+history = model.fit(trainingX, y, epochs=10, validation_split=0.2)
+
+plot_model(model, to_file='D:/UWE-DSP/CNN/Prototype/model.png', show_shapes=True)
